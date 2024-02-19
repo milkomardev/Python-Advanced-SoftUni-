0fe80b6cd52dd5d44d6c82f6231a4da1b5b1080f
milkomardev <122818859+milkomardev@users.noreply.github.com>
2024-02-19 17:57:35 +0200

    Added more solutions

---
+++ tuples_and_sets/exercise/02.sets_of_elements.py
@@ -0,0 +1,9 @@
+n, m = [int(x) for x in input().split()]
+
+first_set = {int(input()) for _ in range(n)}
+second_set = {int(input()) for _ in range(m)}
+
+# print(first_set.intersection(second_set))
+# print(first_set|second_set)
+# print(first_set^second_set)
+print(*first_set&second_set, sep="\n")
